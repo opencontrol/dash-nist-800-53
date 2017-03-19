fc7c5bfad9ca82f86c038605dbfe952146ea084a
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-19 16:41:32 -0400

    remove unnecessary sidebar

--- cleanup.rb
+++ cleanup.rb
@@ -3,6 +3,9 @@ require 'nokogiri'
 Dir.glob('src/beta.nvd.nist.gov/**/*.html') do |filename|
   puts filename
   doc = File.open(filename) { |f| Nokogiri::HTML(f) }
-  doc.css('nav,footer').remove
+
+  doc.css('nav,footer,.col-lg-2').remove
+  doc.css('.col-lg-10').remove_attr('class')
+
   File.open(filename, 'w') { |file| file.write(doc.to_html) }
 end
